dfd9de1082154b3202378634ed555034c67edb37
koodibaas <truan.j.willis@gmail.com>
2022-10-17 08:46:37 +0100

    dic variable name change, word_count function added

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ book_path = "books/frankenstein.txt"
 def main():
     text = open_book(book_path)
     words = get_words(text)
-    characters = get_characters(words)
-    sorted_characters = sort_characters(characters)
+    count = word_count(words)
+    char_dic = get_characters(words)
+    sorted_characters = sort_characters(char_dic)
 
     print(f"--- Begin report of {book_path} ---")
-    print(f"{len(words)} words found in the document")
+    print(f"{count} words found in the document")
     for character in sorted_characters:
-        print(f"The {character} character was found {characters[character]} times")
+        print(f"The {character} character was found {char_dic[character]} times")
     print(f"--- End report ---")
     
 def open_book(path):
@@ -23,20 +24,23 @@ def get_words(text):
     words = text.split()
     return words
 
+def word_count(words):
+    return len(words)
+
 def get_characters(words):
-    characters = {}
+    char_dic = {}
     for word in words:
         word_lowercase = word.lower()
         for character in word_lowercase:
-            if character not in characters:
-                characters[character] = 1
+            if character not in char_dic:
+                char_dic[character] = 1
             else:
-                characters[character] += 1
-    return characters    
+                char_dic[character] += 1
+    return char_dic    
 
-def sort_characters(characters):
+def sort_characters(char_dic):
     sorted_list = [] 
-    for character in characters:
+    for character in char_dic:
         if character.isalpha():
             sorted_list.append(character)
     sorted_list.sort()
